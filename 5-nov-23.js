53581774d5f7a20a1a8505788e7b7a3f4578f925
tanishq singhal <you@example.com>
2024-09-11 16:18:39 +0530

    added 5-nov-23-assignemnt

---
+++ 5-nov-23.js
@@ -0,0 +1,47 @@
+// Question 1: Explain what JavaScript is and its role in web development.
+
+// Javascript is used to add functinality in our webpage ,it is a programming language like other c++ , java ,python.
+
+// Question 2: Explain the key differences between JavaScript and HTML. Provide examples of situations where
+// you would use each.
+
+// HTML is used to structure content on the web. It defines the elements of a webpage, such as headings, paragraphs, images, links, and other content. HTML provides the skeleton of a webpage
+// JavaScript is a programming language used to create dynamic and interactive elements on a webpage. It allows you to manipulate HTML content, respond to user interactions, and control the behavior of the webpage
+
+// Question 3: List and describe the five primitive data types in JavaScript.
+
+// The five primitive data types in JS are = Numbers , strings , boolean , undefines , null 
+
+
+// Question 5: Explain the importance of comments in JavaScript and provide examples of single-line and
+// multi-line comments
+
+// types of Comments in JavaScript
+// JavaScript supports two types of comments: single-line and multi-line comments.
+
+// 1. Single-Line Comments
+// Syntax: Single-line comments start with //. Everything following // on that line is considered a comment and is ignored by the JavaScript engine.
+// This is a single-line comment
+let age = 30; // This is an inline comment explaining the variable
+
+// 2. Multi-Line Comments
+// Syntax: Multi-line comments start with /* and end with */. Everything between these markers is treated as a comment and is ignored by the JavaScript engine.
+
+// Example:
+// /*
+// This is a multi-line comment.
+// It can span multiple lines.
+// Useful for longer explanations or commenting out blocks of code.
+// */
+let salary = 50000; // This is a single-line comment
+
+// Question 6: Explain the importance of choosing meaningful and descriptive variable names in JavaScript.
+// Provide an example where using a clear identifier improves code readability.
+
+// Chossing a meaning full variable names helps us to identify whats conatin in the variable for examble :
+
+let Abooleandatatype = true;
+
+
+
+
